9ce04fe613dbaecfc468a762d662f75013416679
Khoo Ven Jin <venjin_boy@hotmail.com>
2023-11-10 17:20:37 +0800

    refactor(view): re-structure code to utilize product card component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,135 +9,25 @@
     <h2>Recommended</h2>
 
     <div class="recommended">
-      <div class="card">
-        <div class="card-title">Carrots</div>
-        <div class="card-body">
-          <i class="icofont-10x icofont-carrot"></i>
-          <form>
-            <div class="row">
-              <div class="cell">
-                <label>Type:</label>
-              </div>
-              <div class="cell">
-                <em>Vegetable</em>
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Price:</label>
-              </div>
-              <div class="cell">$4.82</div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Quantity:</label>
-              </div>
-              <div class="cell">
-                <input
-                  type="number"
-                  v-model.number="inventory.carrots"
-                />
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <button
-            @click="addToCart('carrots')"
-            class="btn btn-light"
-          >
-            Add to cart
-          </button>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-title">Pineapples</div>
-        <div class="card-body">
-          <i class="icofont-10x icofont-pineapple"></i>
-          <form>
-            <div class="row">
-              <div class="cell">
-                <label>Type:</label>
-              </div>
-              <div class="cell">
-                <em>Fruit</em>
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Price:</label>
-              </div>
-              <div class="cell">$1.62</div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Quantity:</label>
-              </div>
-              <div class="cell">
-                <input
-                  type="number"
-                  v-model="inventory.pineapples"
-                />
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <button class="btn btn-light">Add to cart</button>
-        </div>
-      </div>
-
-      <div class="card">
-        <div class="card-title">Cherries</div>
-        <div class="card-body">
-          <i class="icofont-10x icofont-cherry"></i>
-          <form>
-            <div class="row">
-              <div class="cell">
-                <label>Type:</label>
-              </div>
-              <div class="cell">
-                <em>Fruit</em>
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Price:</label>
-              </div>
-              <div class="cell">$1.04</div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Quantity:</label>
-              </div>
-              <div class="cell">
-                <input
-                  type="number"
-                  v-model="inventory.cherries"
-                />
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <button class="btn btn-light">Add to cart</button>
-        </div>
-      </div>
+      <ProductCard
+        v-for="item in inventory.slice(0, 3)"
+        :key="item.id"
+        class="card"
+        :item="item"
+        :addToCart="addToCart"
+      />
     </div>
   </main>
 </template>
 
 <script>
-import food from '../food.json';
+import ProductCard from '../components/ProductCard.vue';
 
 export default {
   name: 'Home',
-  data() {
-    return {
-      inventory: food
-    };
-  },
-  components: {}
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
 };
 </script>
